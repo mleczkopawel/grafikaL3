300d44064a496572869f4be2d2715547b043499b
entirelilu <ewelinadrewniany@gmail.com>
2017-11-13 12:39:57 +0100

    Zadeklarowanie zmiennych globalnych oraz metod klasy

--- NowyProjekt/Okno.h
+++ NowyProjekt/Okno.h
@@ -1,11 +1,26 @@
 #ifndef OKNO_H
 #define OKNO_H
+
+#include <gl/freeglit.h>
+
 #pragma once
 class Okno
 {
+
+	static int wysokoscOkna;
+	static int szerokoscOkna;
+	int polozenieOknaX;
+	int polozenieOknaY;
+
 public:
 	Okno();
+	Okno(void);
+	Okno(int wysokoscOkna, int szerokoscOkna, int polozenieOknaX, int polozenieOknaY);
 	~Okno();
+	~Okno(void);
+
+	void stworzenieOkna(int argc, char** argv);
+	static void wyswietl();
 };
 #endif //OKNO_H
 
